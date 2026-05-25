d374752a5d006288fe38bb28f99959bdca689127
Ravy1310 <rafiisnanto2005@gmail.com>
2026-05-25 08:19:28 +0700

    dashboard.blade.php: fix pluck() on array dengan wrap collect()   karena controller mengirim \$masaTunggu sebagai PHP array biasa"

--- resources/views/Admin/dashboard.blade.php
+++ resources/views/Admin/dashboard.blade.php
@@ -231,10 +231,10 @@
         new Chart(document.getElementById('chartMasaTunggu'), {
             type: 'bar',
             data: {
-                labels: {!! json_encode($masaTunggu->pluck('label')) !!},
+                labels: {!! json_encode(collect($masaTunggu)->pluck('label')) !!},
                 datasets: [{
                     label: 'Jumlah Alumni',
-                    data: {!! json_encode($masaTunggu->pluck('jumlah')) !!},
+                    data: {!! json_encode(collect($masaTunggu)->pluck('jumlah')) !!},
                     backgroundColor: [
                         'rgba(16, 185, 129, 0.85)',
                         'rgba(59, 130, 246, 0.85)',
